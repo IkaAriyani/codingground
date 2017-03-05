0a2daf8d8cc70ac39c580b6fae1a4993ba3c58da
IkaAriyani <iariyani96@gmail.com>
2017-03-04 23:11:21 -0800

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas8/BiodataTugas8.java
@@ -0,0 +1,10 @@
+public class BiodataTugas8{
+    
+	String nama = "Ika Ariyani";
+	String alamat = "Pengkol Jepara";
+	int umur = 20;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
